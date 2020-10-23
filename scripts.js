7fa6fe322fa5bfd8d47d061f9d672dff4a9fab10
tanamaroby <tanamaroby@gmail.com>
2020-10-23 18:50:13 +0800

    add front-end skeleton

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const app = new VTTCue({
+
+    el: '#main',
+
+    data: {
+        result: "",
+        responseAvailable: false, 
+        apiKey: '<YOUR_RAPIDAPI_KEY'
+    }, 
+
+    methods: {
+        fetchAPIData() {
+
+        }
+    }
+})
